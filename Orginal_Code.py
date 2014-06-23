b412913196d11f4450329a4866323dab6c2fecea
James Robinson <jrobinson@soham-college.org.uk>
2014-06-23 19:21:51 +0000

    Update and rename Orginal_Code to Orginal_Code.py
    re-factored the two email functions into one which recieves a parameter from the main program. This decides which message to use and then sends the email.

--- Orginal_Code.py
+++ Orginal_Code.py
@@ -15,14 +15,13 @@ server.ehlo()
 
 server.login("your_email_address_here", "your_password_here")
 
-def send_an_email_closed():
-    message = "THE DOOR IS CLOSED SAFE" 
+def send_an_email(state):
+    if state ==True:
+        message = "THE DOOR IS CLOSED SAFE"
+    else:
+        message = "THE DOOR IS OPEN QUICK CHECK IT"
     server.sendmail("your_email_address_here", "their_adresss", message)
 
-def send_an_email_open():
-    message = "THE DOOR IS OPEN QUICK CHECK IT"
-    server.sendmail("your_email_address_here", "their_adresss", message)
-    
 while True:
     time_of_event = time.asctime(time.localtime(time.time()))
     if pib.input.a.read()==1:
@@ -30,7 +29,7 @@ while True:
         print "THE DOOR IS NOW CLOSED " + time_of_event
         pib.light.red.off()
         pib.light.green.on()
-        send_an_email_closed()
+        send_an_email(True)
         time.sleep(5)
 
     elif pib.input.a.read()==0:
@@ -38,7 +37,7 @@ while True:
         print"THE DOOR IS OPEN THE CAT/CHILD HAS ESCAPED " + Open_Time
         pib.light.green.off()
         pib.light.red.blink(0.7, 0.3)
-        send_an_email_open()
+        send_an_email(False)
         pib.buzzer.fail()
         time.sleep(5)
         
